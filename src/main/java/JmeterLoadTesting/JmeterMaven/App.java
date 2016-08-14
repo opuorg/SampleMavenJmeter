c5da54c2e92bcf93502b5002f1eb99cee4d867e4
Opu Zaman <Opu.zaman@gmail.com>
2016-08-14 12:43:21 -0400

    initial commit

---
+++ src/main/java/JmeterLoadTesting/JmeterMaven/App.java
@@ -0,0 +1,13 @@
+package JmeterLoadTesting.JmeterMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
